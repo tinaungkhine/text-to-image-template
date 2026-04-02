7157a1e30af250c849d5fed0c31a842f486b8e33
Tin <tinaungkhine@gmail.com>
2026-04-01 22:11:53 -0400

    u

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
 	async fetch(request, env) {
 		const inputs = {
-			prompt: "stunning landscape with mountains and a lake, in the style of a fantasy painting",
+			prompt: "two dogs playing poker",
 		};
 
 		const response = await env.AI.run(
